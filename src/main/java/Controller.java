843109dd58ae5a5803d00e72d92bcc567faad35d
Andrei J <andrei.joldea@student.upt.ro>
2022-05-14 23:59:53 +0300

    Added password field for password

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,13 +1,10 @@
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ProgressBar;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,14 +17,14 @@ public class Controller implements Initializable {
     @FXML
     private TextField tf_username;
     @FXML
-    private TextField tf_password;
+    private PasswordField passwordField;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         button_login.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                DBUtils.loginUser(event, tf_username.getText(), tf_password.getText());
+                DBUtils.loginUser(event, tf_username.getText(), passwordField.getText());
             }
         });
 
