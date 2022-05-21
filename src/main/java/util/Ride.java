b67ba8cac01b83a9ff5b537ec6bba8af70d3c656
Andrei LAZAROV <andrei.lazarov@student.upt.ro>
2022-05-21 22:06:26 +0300

    create branch

--- src/main/java/util/Ride.java
+++ src/main/java/util/Ride.java
@@ -2,18 +2,16 @@ package util;
 
 public class Ride {
     private int id;
-    private String location, destination;
+    private String location, destination, clientName, driverName;
 
-    public Ride(int id, String location, String destination)
-    {
+    public Ride(int id, String location, String destination) {
         this.id = id;
         this.location = location;
         this.destination = destination;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return this.id +" "+ this.location + " " + this.destination;
     }
 }
