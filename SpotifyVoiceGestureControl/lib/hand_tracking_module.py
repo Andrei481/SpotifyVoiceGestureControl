54ccc40aa9921dd0e6891644bc47e44c2077ba3e
Andrei J <andrei.joldea@student.upt.ro>
2023-02-23 13:17:26 +0200

    Added hand tracking module

---
+++ SpotifyVoiceGestureControl/lib/hand_tracking_module.py
@@ -0,0 +1,38 @@
+import cv2
+import mediapipe as mp
+import time
+
+class HandDetector():
+    def __init__(self, static_image_mode=False, max_num_hands=2, model_complexity = 1, detection_confidence=0.5, tracking_confidence=0.5):
+        self.static_image_mode = static_image_mode
+        self.max_num_hands = max_num_hands
+        self.model_complexity = model_complexity
+        self.detection_confidence = detection_confidence
+        self.tracking_confidence = tracking_confidence
+
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(self.static_image_mode, self.max_num_hands, self.model_complexity, self.detection_confidence, self.tracking_confidence)
+        self.mp_drawing = mp.solutions.drawing_utils
+
+    def find_hands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        self.results_landmarks = self.results.multi_hand_landmarks
+        if draw:
+            if self.results_landmarks:
+                for hand_landmark in self.results_landmarks:
+                    self.mp_drawing.draw_landmarks(img, hand_landmark, self.mp_hands.HAND_CONNECTIONS)
+        return img
+
+    def get_landmarks_list(self, img, hand_number=0, draw=True):
+        landmarks_list = []
+        if self.results_landmarks:
+            my_hand = self.results_landmarks[hand_number]
+            for id, landmark in enumerate(my_hand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(landmark.x*w), int(landmark.y*h)
+                print([id, cx, cy])
+                landmarks_list.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 7, (0, 255, 255), cv2.FILLED)
+        return landmarks_list
---
+++ SpotifyVoiceGestureControl/main.py
@@ -0,0 +1,25 @@
+import cv2
+
+from lib.hand_tracking_module import HandDetector
+
+def main():
+    # print("Hello World")
+    # hands = HandDetector()
+    # cap = cv2.VideoCapture(0)
+    # previous_time = current_time = 0
+    # hands = HandDetector()
+    #
+    # while True:
+    #     success, img = cap.read()
+    #     img = cv2.flip(img, 1)  # careful about this and cx, cy -> they are inversed
+    #     img = hands.find_hands(img)
+    #     img.flags.writeable = False
+    #     hands.get_landmarks_list(img)
+    #     cv2.imshow("Image", img)
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit
+    #         break
+    # cap.release()
+    # cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
